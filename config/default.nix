4d9d7c5b0ce433c4a1f55728d7c2aea5d79f6685
D. Sunshine <david@sunshines.org>
2024-09-11 19:53:17 +0000

    Initial customizations
    1. Set leader and local leader
    2. Telescope plugin
    3. Keymap (initially one item for Telescope)

--- config/default.nix
+++ config/default.nix
@@ -2,5 +2,27 @@
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
 
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+  };
+
+  keymaps = [
+    # telescope
+    {
+      options.desc = "Find files";
+      mode = "n";
+      options.silent = true;
+      key = "<leader>ff";
+      action = ":Telescope find_files<cr>";
+    }
+  ];
+
   colorschemes.gruvbox.enable = true;
+
+  plugins = {
+    telescope = {
+      enable = true;
+    };
+  };
 }
