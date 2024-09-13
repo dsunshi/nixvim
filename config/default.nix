1f3bc199b9604c1f0d18c63f7cc44c7b5899d95e
D. Sunshine <david@sunshines.org>
2024-09-13 19:44:36 +0000

    Markdown Preview
    Use non-nix render-markdown.nvim for Markdown support.

--- config/default.nix
+++ config/default.nix
@@ -174,6 +174,14 @@
   extraPlugins = with pkgs; [
     vimPlugins.haskell-tools-nvim
     vimPlugins.nvim-dap
+    (vimUtils.buildVimPlugin {
+        name = "render-markdown.nvim";
+        src = fetchFromGitHub {
+            owner = "MeanderingProgrammer";
+            repo = "render-markdown.nvim";
+            rev = "4bf3247dfd06844088768b37cf1976efef1bca26"; # 7.0.0 release
+            hash = "sha256-U1/6FLCfAkAm692nqMl1qQh6Z1og/Gqoe3NZpa5py9g=";
+        };})
   ];
 
   performance = {
