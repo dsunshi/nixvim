8db47d0b4e5a83592915c9cac003d3c0f02f105f
D. Sunshine <david@sunshines.org>
2024-09-20 17:39:35 +0000

    catppuccin color overrides

--- config/default.nix
+++ config/default.nix
@@ -1,8 +1,14 @@
 { pkgs, ... }: {
   colorschemes.catppuccin.enable = true;
+  colorschemes.catppuccin.settings.flavour = "mocha";
+  colorschemes.catppuccin.settings.color_overrides.mocha = {
+    yellow = "#eba0ac"; # Mocha maroon
+    green = "#94e2d5"; # Mocha teal
+  };
+  
 
   # Import all your configuration modules here
-  #imports = [ ./bufferline.nix ];
+  imports = [ ./bufferline.nix ];
 
   globals = {
     mapleader = " ";
