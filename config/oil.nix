31f96c5c5611dd35411736ed81a293890b75ebf0
D. Sunshine <david@sunshines.org>
2024-09-27 17:05:32 +0000

    Oil tweaks

--- config/oil.nix
+++ config/oil.nix
@@ -7,5 +7,14 @@
     action = "<cmd>Oil<cr>";
   }];
 
-  plugins.oil = { enable = true; };
+  plugins.oil = {
+    enable = true;
+    settings.columns = [ "icon" "permissions" "mtime" ];
+    settings.view_options.show_hidden = false;
+    settings.view_options.is_hidden_file = ''
+      function(name, bufnr)
+         return name ~= ".." and name ~= ".gitignore" and vim.startswith(name, ".")
+      end
+    '';
+  };
 }
