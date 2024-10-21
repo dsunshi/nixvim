dc9c95d4beacd6b7cef9a419cac1638ff15687f3
D. Sunshine <david@sunshines.org>
2024-10-20 20:07:59 -0400

    Remove warnings after flake update

--- config/lsp.nix
+++ config/lsp.nix
@@ -6,12 +6,17 @@
       enable = true;
       inlayHints = true;
       servers = {
-        hls.enable = true;
+        hls = {
+          enable = true;
+          # Do not install ghc via Nixvim. We will prefer to use flakes or 
+          # shell.nix which will give us ghc (with a matching hls of the same version).
+          installGhc = false;
+        };
         clangd.enable = true;
-        openscad-lsp.enable = true;
+        openscad_lsp.enable = true;
         pylsp.enable = true;
         marksman.enable = true;
-        nil-ls = {
+        nil_ls = {
           enable = true;
           settings.formatting.command = [ "nixfmt" ];
         };
