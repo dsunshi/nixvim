792a6c8f83bc72015914cdb6ef4e5b825fbdec83
D. Sunshine <david@sunshines.org>
2024-11-01 18:16:46 -0400

    Ctrl-\ to toggle toggleterm

--- config/toggleterm.nix
+++ config/toggleterm.nix
@@ -1,6 +1,9 @@
 {
   plugins.toggleterm = {
     enable = true;
-    settings.shade_terminals = false;
+    settings = {
+      shade_terminals = false;
+      open_mapping = "[[<c-\\>]]";
+    };
   };
 }
