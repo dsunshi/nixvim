ae475173f828e7e22940125de0f5055755532eda
D. Sunshine <david@sunshines.org>
2024-11-25 10:02:29 -0500

    Noice updates
    Needed to run :TSUpdate before everything worked properly.

--- config/noice.nix
+++ config/noice.nix
@@ -1,5 +1,40 @@
 {
   plugins.noice = {
     enable = true;
+    settings =  { 
+      messages = {
+        enabled = true;
+      };
+      notify = {
+        enabled = false;
+      };
+      lsp = {
+        message = {
+          enabled = true;
+        };
+        progress = {
+          enabled = false;
+          view = "mini";
+        };
+      };
+      popupmenu = {
+        enabled = true;
+        backend = "nui";
+      };
+      format = {
+        filter = {
+          pattern = [
+            ":%s*%%s*s:%s*"
+            ":%s*%%s*s!%s*"
+            ":%s*%%s*s/%s*"
+            "%s*s:%s*"
+            ":%s*s!%s*"
+            ":%s*s/%s*"
+          ];
+          icon = "";
+          # lang = "regex";
+        };
+      };
+    };
   };
 }
